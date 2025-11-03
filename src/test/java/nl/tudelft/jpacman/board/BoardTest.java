8582810dc339876810be338353d49304d4bdfeb6
Aleenta <aleenta.tan@student.mahidol.ac.th>
2025-11-03 23:10:40 +0700

    BoardTest Commit

---
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -0,0 +1,29 @@
+package nl.tudelft.jpacman.board;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class BoardTest {
+
+    @Test
+    void testBoardOneSquare() {
+        Square[][] grid = new Square[1][1];
+        grid[0][0] = new BasicSquare();
+        Board board = new Board(grid);
+
+        assertThat(board.squareAt(0, 0)).isNotNull();
+        assertThat(board.squareAt(0, 0)).isInstanceOf(BasicSquare.class);
+    }
+
+    @Test
+    void testBoardNullSquare() {
+        Square[][] grid = new Square[1][1];
+        grid[0][0] = null;
+        Board board = new Board(grid);
+
+        assertThat(board.invariant()).isFalse();
+        assertThrows(NullPointerException.class, () -> board.squareAt(0,0).getOccupants());
+    }
+}
