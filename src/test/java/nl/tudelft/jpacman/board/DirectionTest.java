98d288ec5eb5d139108770a75cbd3cefdf47cb36
6588159 Aleenta <aleenta.tan@student.mahidol.ac.th>
2025-11-04 15:24:38 +0700

    Update DirectionTest.java

--- src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
+++ src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
@@ -20,21 +20,27 @@ public class DirectionTest {
         assertThat(north.getDeltaX()).isEqualTo(0);
         assertThat(north.getDeltaY()).isEqualTo(-1);
     }
-
+    /**
+     * Do we get the correct delta when moving south?
+     */
     @Test
     void testSouth() {
         Direction south = Direction.valueOf("SOUTH");
         assertThat(south.getDeltaX()).isEqualTo(0);
         assertThat(south.getDeltaY()).isEqualTo(1);
     }
-
+    /**
+     * Do we get the correct delta when moving east?
+     */
     @Test
     void testEast() {
         Direction east = Direction.valueOf("EAST");
         assertThat(east.getDeltaX()).isEqualTo(1);
         assertThat(east.getDeltaY()).isEqualTo(0);
     }
-
+    /**
+     * Do we get the correct delta when moving west?
+     */
     @Test
     void testWest() {
         Direction west = Direction.valueOf("WEST");
