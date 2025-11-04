024ef3c64c13273e984492ade1fb59f7ec1d3aa9
6588159 Aleenta <aleenta.tan@student.mahidol.ac.th>
2025-11-04 15:29:07 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -5,10 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Basic implementation of board.
+ *
+ * @author Jeroen Roosen 
+ */
 public class BoardTest {
     /**
-     * Test board with one square (valid)
-     */
+    * Test board with one square (valid)
+    */
     @Test
     void testBoardOneSquare() {
         Square[][] grid = new Square[1][1];
@@ -19,8 +24,8 @@ public class BoardTest {
         assertThat(board.squareAt(0, 0)).isInstanceOf(BasicSquare.class);
     }
     /**
-     * Test board with null square, squareAt method
-     */
+    * Test board with null square, squareAt method
+    */
     @Test
     void testBoardNullSquare() {
         Square[][] grid = new Square[1][1];
